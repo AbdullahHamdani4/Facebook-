94f5b657643206d8c545b87fca432fe949e42743
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-12-14 18:50:02 +0500

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let form = document.querySelector("form")
 let emailInput = document.querySelector(".emailinput")
 let passInput = document.querySelector(".passinput")
 let errorMsg = document.querySelector(".error")
-let password = true;
+let togglePara=document.getElementById("toggle-para")
 form.addEventListener("submit", (e) => {
+    let email = true;
     errorMsg.style.display = "none"
     e.preventDefault()
     //Email checking 
@@ -14,6 +15,7 @@ form.addEventListener("submit", (e) => {
             text: "Email is empty",
             icon: "question"
         })
+        email=false
     }
     //else is checking whether it contains @ and .
     else {
@@ -22,21 +24,52 @@ form.addEventListener("submit", (e) => {
         for (let i = 0; i < emailInput.value.length; i++) {
             if (emailInput.value[i] == "@") {
                 flag1 = false
-                password = false;
             }
             if (emailInput.value[i] == ".") {
                 flag2 = false
-                password = false;
             }
         }
         //Checking @
         if (flag1) {
             errorMsg.style.display = "block"
+            email = false;
         }
         //Checking .
         else if (flag2) {
             errorMsg.innerText = "Email should contain ."
             errorMsg.style.display = "block"
+            email = false;
+        }
+     }
+    //Password checking 
+    if (email) {
+        //Checking whether its empty or not
+        if (passInput.value.trim() == "") {
+            Swal.fire({
+                title: "Enter Password",
+                text: "Password is empty",
+                icon: "question"
+            })
+        }
+        //Checking the length whether its 7 or greater
+        else if (passInput.value.trim().length < 7) {
+            Swal.fire({
+                title: "Password is Short",
+                text: "Password should contain atleast 7 characters",
+                icon: "warning"
+            })
+        }
+        //All conditions met so redirect it
+        else {
+            Swal.fire({
+                title: "Login Successfully",
+                text: "Redirecting you to Dasboard",
+                icon: "success"
+            }).then((result)=>{
+              if(result.isConfirmed){
+                window.location.href="http://127.0.0.1:5500/dashboard.html"
+              }
+            })
         }
     }
 })
@@ -83,36 +116,7 @@ form.addEventListener("submit", (e) => {
 
 
 
-// else if (passInput.value.trim() == "") {
-//     Swal.fire({
-//         title: "Enter Password",
-//         text: "Password is empty",
-//         icon: "question"
-//     })
-// }
-// else if (passInput.value.trim().length < 7) {
-//     Swal.fire({
-//         title: "Password is Short",
-//         text: "Password should be atleast 7 characters",
-//         icon: "warning"
-//     })
-// }
 
 
 
 
-
-
-
-
-
-
-// function removeError(this) {
-//     for (let i = 0; i < this.value.length; i++) {
-//             if (this.value[i] == "@") {
-//                 // flag = false
-//                 errorMsg.style.display="none"
-
-//             }
-//         }
-// }
