271c25e838d6fbc719e39185bdfe02e384809655
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-12-14 15:34:26 +0500

    Add email validation and error handling to login form

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ let form = document.querySelector("form")
 let emailInput = document.querySelector(".emailinput")
 let passInput = document.querySelector(".passinput")
 let errorMsg = document.querySelector(".error")
+let password = true;
 form.addEventListener("submit", (e) => {
-    errorMsg.style.display="none"
+    errorMsg.style.display = "none"
     e.preventDefault()
+    //Email checking 
+    //if is checking whether its empty or nor
     if (emailInput.value.trim() == "") {
         Swal.fire({
             title: "Enter Email",
@@ -12,35 +15,104 @@ form.addEventListener("submit", (e) => {
             icon: "question"
         })
     }
-
+    //else is checking whether it contains @ and .
     else {
-        // console.log( emailInput.length);
-        // console.log(object);
-        let flag = true;
+        let flag1 = true;
+        let flag2 = true;
         for (let i = 0; i < emailInput.value.length; i++) {
             if (emailInput.value[i] == "@") {
-                flag = false
+                flag1 = false
+                password = false;
+            }
+            if (emailInput.value[i] == ".") {
+                flag2 = false
+                password = false;
             }
         }
-        if (flag) {
-            errorMsg.style.display="block"
+        //Checking @
+        if (flag1) {
+            errorMsg.style.display = "block"
+        }
+        //Checking .
+        else if (flag2) {
+            errorMsg.innerText = "Email should contain ."
+            errorMsg.style.display = "block"
         }
     }
-
-    // else if (passInput.value.trim() == "") {
-    //     Swal.fire({
-    //         title: "Enter Password",
-    //         text: "Password is empty",
-    //         icon: "question"
-    //     })
-    // }
-    // else if (passInput.value.trim().length < 7) {
-    //     Swal.fire({
-    //         title: "Password is Short",
-    //         text: "Password should be atleast 7 characters",
-    //         icon: "warning"
-    //     })
-    // }
-
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// else if (passInput.value.trim() == "") {
+//     Swal.fire({
+//         title: "Enter Password",
+//         text: "Password is empty",
+//         icon: "question"
+//     })
+// }
+// else if (passInput.value.trim().length < 7) {
+//     Swal.fire({
+//         title: "Password is Short",
+//         text: "Password should be atleast 7 characters",
+//         icon: "warning"
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+// function removeError(this) {
+//     for (let i = 0; i < this.value.length; i++) {
+//             if (this.value[i] == "@") {
+//                 // flag = false
+//                 errorMsg.style.display="none"
+
+//             }
+//         }
+// }
