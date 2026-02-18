69a683c93acedbf25aec248e3d566f88e7272e38
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-02-18 16:15:43 +0500

    Add posts data structure for current user with profile and engagement details

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -50,6 +50,49 @@ let storiesOfCurrentUser = [
         imageUrl: "https://picsum.photos/300/500?random=10",
     }
 ];
+let postsOfCurrentUser = [
+    {
+        profilePic: "https://randomuser.me/api/portraits/men/21.jpg",
+        profileName: "Ali Khan",
+        dateTime: "14 Feb at 23:57",
+        contentPic: "https://picsum.photos/600/400?random=41",
+        comments: 12,
+        shares: 3
+    },
+    {
+        profilePic: "https://randomuser.me/api/portraits/women/22.jpg",
+        profileName: "Sara Ahmed",
+        dateTime: "13 Feb at 18:22",
+        contentPic: "https://picsum.photos/600/400?random=42",
+        comments: 8,
+        shares: 5
+    },
+    {
+        profilePic: "https://randomuser.me/api/portraits/men/23.jpg",
+        profileName: "Usman Tariq",
+        dateTime: "12 Feb at 09:15",
+        contentPic: "https://picsum.photos/600/400?random=43",
+        comments: 25,
+        shares: 10
+    },
+    {
+        profilePic: "https://randomuser.me/api/portraits/women/24.jpg",
+        profileName: "Ayesha Noor",
+        dateTime: "11 Feb at 14:40",
+        contentPic: "https://picsum.photos/600/400?random=44",
+        comments: 4,
+        shares: 1
+    },
+    {
+        profilePic: "https://randomuser.me/api/portraits/men/25.jpg",
+        profileName: "Hamza Ali",
+        dateTime: "10 Feb at 20:10",
+        contentPic: "https://picsum.photos/600/400?random=45",
+        comments: 19,
+        shares: 7
+    }
+];
+
 let storiesSection=document.querySelector(".stories-section")
 let createStoriesThroughMap=
     storiesOfCurrentUser.map(story=>{
@@ -63,3 +106,9 @@ let createStoriesThroughMap=
     `
 });
 storiesSection.innerHTML+=createStoriesThroughMap.join("")
+
+
+    
+
+
+
